5838a89d72dcf236777b0cc0bac75070afbef449
Jay Stevens <disneylandjay@gmail.com>
2017-12-05 19:02:20 -0800

    Fixed term.

--- predictive_table.py
+++ predictive_table.py
@@ -3,7 +3,6 @@ terminal_list = [
         "program",
         ";",
         "var",
-        "dec_list",
         "begin",
         "end.",
         ":",
@@ -53,7 +52,9 @@ non_terminals = [
         "dec",
         "assign",
         "dec-list",
+        "dec-prime",
         "stat-list",
+        "stat-list-prime",
         "term",
         "expr"
     ]
@@ -63,12 +64,16 @@ table = {}
 for non_terminal in non_terminals:
     non_terminal_terminals = {}
     for terminal in terminal_list:
-        non_terminal_terminals[terminal] = "Error"
+        non_terminal_terminals[terminal] = "!"
     table[non_terminal] = non_terminal_terminals
 
+def add_to_table(key, list_of_intersections, value):
+    for intersection in list_of_intersections:
+        table[key][intersection] = value
+
 # Prog
-table['prog']['program'] = "program id ; var dec_list begin stat-list end."
-table['prog']['$'] = "program id ; var dec_list begin stat-list end."
+table['prog']['program'] = "program id ; var dec-list begin stat-list end."
+table['prog']['$'] = "program id ; var dec-list begin stat-list end."
 
 # ID
 table['id']['p'] = table['id']['q'] = table['id']['r'] = table['id']['s'] = "letter id-after"
@@ -85,4 +90,45 @@ table['id-after']['='] = table['id-after'][');'] = table['id-after'][','] = tabl
 # Dec-List
 table['dec-list']['p'] = table['dec-list']['q'] = table['dec-list']['r'] = table['dec-list']['s'] = "dec : type ;"
 
-print(table)
+# Dec
+table['dec']['p'] = table['dec']['q'] = table['dec']['r'] = table['dec']['s'] = "id , dec-prime"
+
+# Dec-Prime
+table['dec-prime']['p'] = table['dec-prime']['q'] = table['dec-prime']['r'] = table['dec-prime']['s'] = "id , dec-prime"
+table['dec-prime'][':'] = ""
+
+# Type
+table['type']['integer'] = "integer"
+
+# Stat-List
+table['stat-list']['print('] = table['stat-list']['p'] = table['stat-list']['q'] = \
+    table['stat-list']['r'] = table['stat-list']['s'] = "stat stat-list-prime"
+
+# Stat-List Prime
+table['stat-list-prime']['print('] = table['stat-list-prime']['p'] = table['stat-list-prime']['q'] = \
+    table['stat-list-prime']['r'] = table['stat-list-prime']['s'] = "stat stat-list-prime"
+table['stat-list-prime']['end.'] = ""
+
+# Stat
+table['stat']['print('] = "write"
+table['stat']['p'] = table['stat']['q'] = table['stat']['r'] = table['stat']['s'] = "assign"
+
+# Write
+table['write']['print('] = "print( id );"
+
+# Assign
+add_to_table('assign', ['p', 'q', 'r', 's'], "id = expr")
+
+# Expr
+add_to_table('expr', ['(', 'p', 'q', 'r', 's', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], "term expr-prime")
+
+# Expr-Prime
+add_to_table('expr-prime', ['+'], "+ term expr-prime")
+add_to_table('expr-prime', ['-'], "- term expr-prime")
+add_to_table('expr-prime', [')', ';'], "")
+
+# Term
+add_to_table('term', ['(', 'p', 'q', 'r', 's', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], "factor term-prime")
+
+for entry in table:
+    print(entry + ": " + str(table[entry]))
